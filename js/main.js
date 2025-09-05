05e68c13fd609f52f991ec8b608b780ead9df4d1
Linkjustice2 <gdbacgdbac@gmail.com>
2025-09-05 18:03:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+let pebbles = 0;
+
+const oreCount = document.getElementById("ore-count");
+const mineBtn = document.getElementById("mine-btn");
+
+mineBtn.addEventListener("click", () => {
+  pebbles++;
+  oreCount.textContent = `Pebbles: ${pebbles}`;
+});
